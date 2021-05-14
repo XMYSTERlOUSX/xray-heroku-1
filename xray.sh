c706d57162f98ae185c7c955a722fec2b9072b55
iamtrazy <iamtrazy@protonmail.com>
2021-05-14 19:35:48 +0530

    Add files via upload

---
+++ xray.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# config xray
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+        {
+            "port": $PORT,
+            "protocol": "$PROTOCOL",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID", 
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": { 
+                    "path": "/websocket"
+                }
+            }
+        }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
