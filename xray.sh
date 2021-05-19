6f421a189a034f0fe006399494dc86744b539a17
iamtrazy <iamtrazy@protonmail.com>
2021-05-19 09:34:54 +0530

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,37 +1,55 @@
 #!/bin/sh
 
 # config xray
+mkdir /opensssl && cd /opensssl
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
+    -keyout xray.key  -out xray.crt
+chmod 777 xray.key
 cat << EOF > /etc/config.json
 {
-  "inbounds":[
+    "inbounds": [
         {
             "port": $PORT,
             "protocol": "$PROTOCOL",
             "settings": {
                 "clients": [
                     {
-                        "id": "$UUID", 
-                        "level": 0,
-                        "email": "love@example.com"
+                        "id": "$UUID",
+                        "flow": "xtls-rprx-direct",
+                        "level": 0
                     }
                 ],
-                "decryption": "none"
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": "80"
+                    }
+                 ]
             },
             "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": { 
-                    "path": "/websocket"
+                "network": "tcp",
+                "security": "xtls",
+                "xtlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/opensssl/xray.crt",
+                            "keyFile": "/openssl/xray.key"
+                        }
+                    ]
                 }
             }
         }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    }
-  ]
-}	
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
 EOF
 
 # run xray
